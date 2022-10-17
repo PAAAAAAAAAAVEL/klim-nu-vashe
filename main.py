70f0561f4f26f25c2e3cc88f84b9aa68d082ace7
Pavel Miheev <114918616+PAAAAAAAAAAVEL@users.noreply.github.com>
2022-10-17 11:48:45 +0000

    d

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
-men=int(input("Введи число, а я покажу, какой месяц ему соответствует: "))
-if men>12:
-  print("Такого месяца нет")
-if men<1:
-  print("Такого месяца нет")
-if men==1:print("январь")
-elif men==2:print("Февраль")
-elif men==3:print("Март")
-elif men==4:print("Апрель")
-elif men==5:print("Май")
-elif men==6:print("Июнь")
-elif men==7:print("Июль")
-elif men==8:print("Август")
-elif men==9:print("Сентябрь")
-elif men==10:print("Октябрь")
-elif men==11:print("Ноябрь")
-elif men==12:print("Декабрь")
+# men=int(input("Введи число, а я покажу, какой месяц ему соответствует: "))
+# if men>12:
+#   print("Такого месяца нет")
+# if men<1:
+#   print("Такого месяца нет")
+# if men==1:print("январь")
+# elif men==2:print("Февраль")
+# elif men==3:print("Март")
+# elif men==4:print("Апрель")
+# elif men==5:print("Май")
+# elif men==6:print("Июнь")
+# elif men==7:print("Июль")
+# elif men==8:print("Август")
+# elif men==9:print("Сентябрь")
+# elif men==10:print("Октябрь")
+# elif men==11:print("Ноябрь")
+# elif men==12:print("Декабрь")
+
+# print("ievadi 3 leņķus")
+# a=int(input("Ievadiet 1. leņķ: "))
+# b=int(input("Ievadiet 2. leņķ: "))
+# c=int(input("Ievadiet 3. leņķ: "))
+# t=a+b+c
+# if t>180:
+#   print("tristuris neeksiste")
+# if t<180:
+#   print("tristuris neeksiste")
+# elif a+b+c==180:
+#   print("tristuris eksiste")
+# if a==b==c: print("tas ir vienadmalu tristuris")
+# elif a==90: print("tas ir taisnleņķa trijstūris")
+# elif b==90: print("tas ir taisnleņķa trijstūris")
+# elif c==90: print("tas ir taisnleņķa trijstūris")
+# if a>90: print("tas ir platleņķa trijstūris")
+# elif b>90: print("tas ir platleņķa trijstūris")
+# elif c>90: print("tas ir platleņķa trijstūris")
+# if a<90 and b<90 and c<90: print("tas ir šaurleņķa trijstūris")
+
+# sk=int(input("введите число "))
+# if sk<0:
+#   print("это негативное число")
+# if sk>0:
+#   print("это позитивное число")
+# if sk<-999: print("четырёх(или более)значное число")
+# elif sk<-99: print("трёхзначное число")
+# elif sk<-9: print("двухзначное число")
+# elif sk>999: print("четырёх(или более)значное число")
+# elif sk>99: print("трёхзначное число")
+# elif sk>9: print("двухзначное число")
+# elif sk>0 and sk<10 and sk>-10: print("однозначное число")
+
+# x=int(input("Введи x: "))
+# y=int(input("Введи y: "))
+# if x==0 and y==0:
+#   print("точка находится в начале оси координат")
+# if x==0:
+#   print("точка находится на оси y")
+# if y==0:
+#   print("точка находится на оси x")
+# if x>0 and y>0: print("1 квадрант")
+# elif x<0 and y>0: print("2 квадрант")
+# elif x<0 and y<0: print("3 квадрант")
+# elif x>0 and y<0: print("4 квадрант")
